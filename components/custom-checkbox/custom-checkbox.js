1683b419156b64becbe87108db13bcb26f1ec3e3
Maxim Zozulya <m.zozulya@systtech.ru>
2024-11-21 23:23:20 +0200

    fix: updated state for role

--- components/custom-checkbox/custom-checkbox.js
+++ components/custom-checkbox/custom-checkbox.js
@@ -21,33 +21,31 @@ class CustomCheckbox extends HTMLElement {
     this.addEventListener("click", this.toggleState.bind(this));
   }
 
-
+  // Вызывается после добавления компонента в DOM
   connectedCallback() {
     if (!this.hasAttribute("role")) {
       this.setAttribute("role", "unchecked");
     }
-    
+
     if (this.hasAttribute("size")) {
       this._updateSize(this.getAttribute("size"));
     }
-    
-    if (this.hasAttribute("role")) {
-      this.setAttribute("role", this.getAttribute("role"));
-    }
   }
 
-
+  // Возвращает массив атрибутов, за изменениями которых следует наблюдать
   static get observedAttributes() {
-    return ["checked", "size"];
+    return ["size", "role"];
   }
 
-
+  // Вызывается при изменении наблюдаемых атрибутов
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name === "checked") {
-      this.checkboxState = newValue ? "checked" : "unchecked";
-      this._updateState();
-    } else if (name === "size") {
+    if (name === "size") {
       this._updateSize(newValue);
+    }
+    if (name === "role") {
+      if (this.internals_ && this.internals_.states) {
+        this._updateRole(newValue)
+      }
     }
   }
 
@@ -59,6 +57,13 @@ class CustomCheckbox extends HTMLElement {
       div.style.width = `${newSize}px`;
       div.style.height = `${newSize}px`;
     }
+  }
+
+  _updateRole(role) {
+    this.internals_.states.delete("checked");
+    this.internals_.states.delete("mixed");
+    this.internals_.states.delete("unchecked");
+    this.internals_.states.add(role);
   }
 
 
@@ -77,30 +82,33 @@ class CustomCheckbox extends HTMLElement {
     this._updateState();
   }
 
-
+  // Обновляет состояние компонента
   _updateState() {
+    // Применяет текущее состояние в aria-checked и в CustomStateSet
     if (this.internals_ && this.internals_.states) {
       this.internals_.states.delete("checked");
       this.internals_.states.delete("mixed");
       this.internals_.states.delete("unchecked");
 
-      console.log(123)
       if (this.checkboxState) {
-          this.internals_.states.add(this.checkboxState);
-          return this.setAttribute("role", this.checkboxState);
+        this.internals_.states.add(this.checkboxState);
+        return this.setAttribute("role", this.checkboxState);
       }
 
-      if(this.hasAttribute("role")){
-          this.checkboxState = this.getAttribute("role");
-          this.internals_.states.add(this.getAttribute("role"));
-         return this.setAttribute("role", this.checkboxState);
+      if (this.hasAttribute("role")) {
+        this.checkboxState = this.getAttribute("role");
+        this.internals_.states.add(this.getAttribute("role"));
+        return this.setAttribute("role", this.checkboxState);
       }
+
       this.internals_.states.add("unchecked");
       this.checkboxState = "unchecked";
     }
   }
 
+  // Ленивая инициализация attachInternals для использования Custom Elements API
   get internals_() {
+    // Возвращает внутренние API для управления состояниями элемента
     if (!this._internals) {
       this._internals = this.attachInternals();
     }
